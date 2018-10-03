1213bb481838a9d0f03e59e2eb53a56021aa0de6
kz <m13715194169_1@163.com>
2018-10-03 14:31:06 +0800

    feat: add RfModel

---
+++ lgoods/src/models/refund/RfModel.php
@@ -0,0 +1,12 @@
+<?php
+namespace lgoods\models\order;
+
+use Yii;
+use lgoods\models\goods\GoodsModel;
+use yii\base\Model;
+use yii\helpers\ArrayHelper;
+use lgoods\models\order\AfterPayedEvent;
+
+class RfModel extends Model{
+
+}
