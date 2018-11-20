6a32af6b7850b74e8c2117ef5db3b1e0cf516cac
kz <784248377@qq.com>
2018-11-20 17:26:07 +0800

    fix: 处理跨域

--- lbase/src/Controller.php
+++ lbase/src/Controller.php
@@ -9,6 +9,27 @@ use yii\web\Controller as BaseController;
 class Controller extends BaseController
 {
     public $enableCsrfValidation = false;
+
+    public function behaviors(){
+        return [
+            'corsFilter' => [
+                'class' => \yii\filters\Cors::className(),
+                'cors' => [
+                    // restrict access to
+                    'Origin' => ["*"],
+                    'Access-Control-Request-Method' => ['POST', 'PUT', 'GET', 'DELETE', 'OPTION'],
+                    // Allow only POST and PUT methods
+                    'Access-Control-Request-Headers' => ['X-Wsse'],
+                    // Allow only headers 'X-Wsse'
+                    'Access-Control-Allow-Credentials' => true,
+                    // Allow OPTIONS caching
+                    'Access-Control-Max-Age' => 3600,
+                    // Allow the X-Pagination-Current-Page header to be exposed to the browser.
+                    'Access-Control-Expose-Headers' => ['X-Pagination-Current-Page'],
+                ],
+            ],
+        ];
+    }
     private function getRes(){
         return [
             'code' => null,
